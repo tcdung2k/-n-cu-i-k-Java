4c34d564bd00f929e456c5fd50f073f3ad5fd81a
danghoanglong719 <danghoanglong1919@gmail.com>
2021-11-22 06:57:06 -0800

    DAO

---
+++ RestaurantManagerment/src/main/java/com/mycompany/restaurantmanagerment/DAO/CategoriesDAO.java
@@ -0,0 +1,162 @@
+package com.mycompany.restaurantmanagerment.DAO;
+
+import com.mycompany.restaurantmanagerment.Controller.ConnectDB;
+import com.mycompany.restaurantmanagerment.Modals.Category;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriesDAO {
+
+    Category category;
+    ConnectDB conn;
+
+    public CategoriesDAO() {
+        conn = new ConnectDB();
+    }
+
+    //    Get all Category
+//    return List Category
+    public List<Category>getAllCategories () {
+
+        List<Category> lstCategory = new ArrayList<>();
+
+        conn.getConnect();
+
+        try {
+            Statement stmt = conn.connect.createStatement();
+
+            String query = "SELECT * FROM Categories";
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                int Id = rs.getInt("Id");
+                String name = rs.getString("Name");
+
+                Category cate = new Category(Id, name);
+
+                lstCategory.add(cate);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Lỗi truy vấn");
+        } finally {
+            conn.closeConnect();
+        }
+
+        return lstCategory;
+    }
+
+    //     insert Category
+//    param String name
+    public void InsertCategory(String name) {
+
+        conn.getConnect();
+
+        try {
+
+            String query = "INSERT INTO Categories VALUES(?)";
+
+            PreparedStatement stmt = conn.connect.prepareStatement(query);
+            stmt.setString(1, name);
+
+            stmt.executeUpdate();
+
+            System.out.println("Inserted: " + name);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Lỗi truy vấn");
+        } finally {
+            conn.closeConnect();
+        }
+
+    }
+
+    //    get category by id
+    public Category getCategorybyId(int id) {
+
+        conn.getConnect();
+
+        Category cate = new Category();
+
+        try {
+            Statement stmt = conn.connect.createStatement();
+            String query = "SELECT * FROM Categories WHERE Id = " + id;
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                int Id = rs.getInt("Id");
+                String name = rs.getString("Name");
+
+                cate = new Category(Id, name);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Lỗi truy vấn");
+        } finally {
+            conn.closeConnect();
+        }
+
+        return cate;
+    }
+
+//    delete category
+    public void deleteCategoryById(int id){
+        conn.getConnect();
+
+        try {
+
+            String query = "DELETE FROM Categories WHERE Id =  ?";
+
+            PreparedStatement stmt = conn.connect.prepareStatement(query);
+
+            stmt.setInt(1,id);
+
+            stmt.executeUpdate();
+
+            System.out.println("Deleted: " + id);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Lỗi truy vấn");
+        } finally {
+            conn.closeConnect();
+        }
+    }
+
+//    update category
+
+    public void updateCategoryById(int id, String name){
+
+        conn.getConnect();
+
+        try {
+
+            String query = "UPDATE Categories SET  NAME = ? WHERE Id =  ?";
+
+            PreparedStatement stmt = conn.connect.prepareStatement(query);
+            stmt.setString(1,name);
+            stmt.setInt(2,id);
+
+            stmt.executeUpdate();
+
+            System.out.println("Update complete " + id);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Lỗi truy vấn");
+        } finally {
+            conn.closeConnect();
+        }
+
+    }
+
+}
